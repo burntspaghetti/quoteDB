84523e8ddd5a44ef7a75ff944b0971c7bae8f51c
singularitarian <nsbertanzetti@crimson.ua.edu>
2015-05-09 20:16:35 -0500

    init

---
+++ app/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+
+@section('content')
+
+
+        {{--<div align="center" >--}}
+            {{--<img src="{{asset('images/mtn.jpg')}}" style="height: 10em; border-radius: 25px 25px 25px 25px;"/>--}}
+        {{--</div>--}}
+
+
+    </div>
+@endsection
