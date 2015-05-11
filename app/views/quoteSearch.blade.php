dff9d4250954b591d4e573b6176ecf9e7cc1b99c
singularitarian <nsbertanzetti@crimson.ua.edu>
2015-05-11 18:48:40 -0500

    search results

--- app/views/quoteSearch.blade.php
+++ app/views/quoteSearch.blade.php
@@ -4,25 +4,38 @@
 
 
 
-    <table id="table_id" class="display">
-        <thead>
-        <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 2</td>
-        </tr>
-        <tr>
-            <td>Row 2 Data 1</td>
-            <td>Row 2 Data 2</td>
-        </tr>
-        </tbody>
-    </table>
+    {{--<table id="table_id" class="table">--}}
+        {{--<thead>--}}
+        {{--<tr>--}}
+            {{--<th>Quote</th>--}}
+            {{--<th>Persona</th>--}}
+        {{--</tr>--}}
+        {{--</thead>--}}
+        {{--<tbody>--}}
 
+        {{--@foreach($quotes as $quote)--}}
+            {{--<tr>--}}
+                {{--<td>Row 1 Data 1</td>--}}
+                {{--<td>Row 1 Data 2</td>--}}
+            {{--</tr>--}}
+        {{--@endforeach--}}
+
+        {{--</tbody>--}}
+    {{--</table>--}}
+
+
+    <div class="row">
+        @foreach($quotes as $quote)
+            <div class="col-lg-6">
+                <div class="bs-component">
+                    <blockquote>
+                        <p>{{$quote->quoteText}}</p>
+                        <small>{{$personaArray[$quote->idPersona]->fName . " " . $personaArray[$quote->idPersona]->lName}} <cite title="Source Title">Source Title</cite></small>
+                    </blockquote>
+                </div>
+            </div>
+        @endforeach
+    </div>
 
     <script>
         $(document).ready( function () {
