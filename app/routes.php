8781555641139d641c59369493efcef049ab6e8f
singularitarian <nsbertanzetti@crimson.ua.edu>
2015-05-20 17:02:04 -0500

    refactor routes

--- app/routes.php
+++ app/routes.php
@@ -2,37 +2,37 @@
 
 Route::group(array('before' => 'auth'), function()
 {
-	Route::get('/quoteDB', 'HomeController@home');
+	Route::get('/', 'HomeController@home');
 
-	Route::post('/quoteDB/search', 'HomeController@search');
+	Route::post('/search', 'HomeController@search');
 
-	Route::get('/quoteDB/searchResults', 'HomeController@searchResults');
-
-
-	Route::get('/quoteDB/personas', 'PersonaController@personas');
-
-	Route::get('/quoteDB/persona/{idPersona}', 'PersonaController@showPersona');
-
-	Route::get('/quoteDB/personas/create', 'PersonaController@create');
-
-	Route::post('quoteDB/personas/store', 'PersonaController@store');
-
-	Route::get('/quoteDB/persona/edit/{id}', 'PersonaController@edit');
-
-	Route::post('/quoteDB/persona/update/{id}', 'PersonaController@update');
-
-	Route::get('quoteDB/persona/delete/{id}', 'PersonaController@delete');
+	Route::get('/searchResults', 'HomeController@searchResults');
 
 
-	Route::get('/quoteDB/persona/quotes/create/{idPersona}', 'QuoteController@create');
+	Route::get('/personas', 'PersonaController@personas');
 
-	Route::post('/quoteDB/persona/quotes/store', 'QuoteController@store');
+	Route::get('/persona/{idPersona}', 'PersonaController@showPersona');
 
-	Route::get('/quoteDB/persona/quotes/edit/{idPersona}/{idQuote}', 'QuoteController@edit');
+	Route::get('/personas/create', 'PersonaController@create');
 
-	Route::post('/quoteDB/persona/quotes/saveEdit', 'QuoteController@update');
+	Route::post('/personas/store', 'PersonaController@store');
 
-	Route::get('/quoteDB/persona/quotes/delete/{idQuote}', 'QuoteController@delete');
+	Route::get('/persona/edit/{id}', 'PersonaController@edit');
+
+	Route::post('/persona/update/{id}', 'PersonaController@update');
+
+	Route::get('/persona/delete/{id}', 'PersonaController@delete');
+
+
+	Route::get('/persona/quotes/create/{idPersona}', 'QuoteController@create');
+
+	Route::post('/persona/quotes/store', 'QuoteController@store');
+
+	Route::get('/persona/quotes/edit/{idPersona}/{idQuote}', 'QuoteController@edit');
+
+	Route::post('/persona/quotes/saveEdit', 'QuoteController@update');
+
+	Route::get('/persona/quotes/delete/{idQuote}', 'QuoteController@delete');
 
 });
 
