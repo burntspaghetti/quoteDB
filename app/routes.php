4dbab267cdc68e9ecaa56eca00fb80bfd716eb10
singularitarian <nsbertanzetti@crimson.ua.edu>
2015-06-04 19:01:11 -0500

    clean more

--- app/routes.php
+++ app/routes.php
@@ -1,41 +1,4 @@
 <?php
-use Goutte\Client;
-
-Route::get('/test', function()
-{
-	$client = new Client();
-
-	$crawler = $client->request('GET', 'http://en.wikiquote.org/wiki/Thomas_Jefferson');
-
-	$quotes = $crawler->filter('ul > li');
-
-	$quoteArray = [];
-
-	foreach($quotes as $quote)
-	{
-		if(strpos($quote->nodeValue, 'Create account') !== false)
-		{
-			break;
-		}
-		elseif(!is_numeric(substr($quote->nodeValue, 0, 1)))
-		{
-			array_push($quoteArray, $quote->nodeValue);
-		}
-	}
-
-	$quoteCount = count($quoteArray);
-	for($x = 0; $x < $quoteCount - 1; $x++)
-	{
-		if(strpos($quoteArray[$x], $quoteArray[$x + 1]) !== false)
-		{
-			unset($quoteArray[$x+1]);
-			$quoteArray = array_values($quoteArray);
-			$quoteCount = count($quoteArray);
-		}
-	}
-
-	return View::make('quoteFilter')->with('quoteArray', $quoteArray);
-});
 
 Route::group(array('before' => 'auth'), function()
 {
